dc6ed2ef7961d22e6eddba60ce0069abd3e58f6e
Mikhail <113991682+OkeyNirvana@users.noreply.github.com>
2026-01-04 19:14:04 +0300

    Начальный коммит проекта OnlineStore

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+using System.Text.Json;
+using System.Text;
+
+namespace OnlineStore.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public string Index()
+    {
+        {
+    var path = Path.Combine(
+        Directory.GetCurrentDirectory(),
+        "data",
+        "Products.json"
+    );
+
+    var json = System.IO.File.ReadAllText(path);
+
+    var products = JsonSerializer.Deserialize<List<Product>>(json);
+
+    var sb = new StringBuilder();
+
+    foreach (var product in products)
+    {
+        sb.AppendLine(product.Id.ToString());
+        sb.AppendLine(product.Name);
+        sb.AppendLine(product.Cost.ToString());
+        sb.AppendLine(); 
+    }
+    return sb.ToString();
+    }
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
